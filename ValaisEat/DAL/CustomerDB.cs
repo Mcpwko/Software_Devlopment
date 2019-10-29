54e5b9136599a881df571c4fd7fd53eb3e10934d
jacket28 <kevin.coppey28@hotmail.com>
2019-10-29 14:58:42 +0100

    rset-èrtesretytes

---
+++ ValaisEat/DAL/CustomerDB.cs
@@ -0,0 +1,209 @@
+﻿using DTO;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DAL
+{
+    public class CustomerDB : ICustomerDB
+    {
+        public IConfiguration Configuration { get; }
+        public CustomerDB(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        public Customer GetCustomer(int id)
+        {
+            Customer customer = null;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Customer WHERE IdClient = @id";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+
+                            customer = new Customer();
+
+                            customer.IdClient = (int)dr["IdClient"];
+                            customer.IdUser = (int)dr["IdUser"];
+
+
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return customer;
+
+        }
+
+
+
+
+
+        public List<Customer> GetCustomers()
+        {
+            List<Customer> results = null;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Customer";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+
+                    cn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            if (results == null)
+                                results = new List<Customer>();
+
+                            Customer customer = new Customer();
+
+                            customer.IdClient = (int)dr["IdClient"];
+                            customer.IdUser = (int)dr["IdUser"];
+
+
+                            results.Add(customer);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return results;
+        }
+
+
+        public Customer AddCustomer(Customer customer)
+        {
+
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT into Customer(IdClient,IdUser) VALUES(@IdClient,@IdUser);SELECT SCOPE_IDENTITY()";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+
+
+
+                    cmd.Parameters.AddWithValue("@IdClient", customer.IdClient);
+                    cmd.Parameters.AddWithValue("@IdUser", customer.IdUser);
+
+
+                    cn.Open();
+
+                    customer.IdClient = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return customer;
+
+
+
+        }
+
+
+        public int UpdateCustomer(Customer customer)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            int result = 0;
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+
+                    string query = "UPDATE Customer SET IdClient=@IdClient,IdUser=@IdUser WHERE IdClient=@IdClient";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+
+
+                    cmd.Parameters.AddWithValue("@IdClient", customer.IdClient);
+                    cmd.Parameters.AddWithValue("@IdUser", customer.IdUser);
+          
+                    cn.Open();
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return result;
+
+        }
+
+
+        public int DeleteCustomer(int IdClient)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            int result = 0;
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+
+                    string query = "DELETE FROM Customer WHERE IdClient=@IdClient";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@IdClient", IdClient);
+
+                    cn.Open();
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return result;
+
+        }
+
+        public Customer AddCustomer(Restaurant restaurant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteRestaurant(int idRestaurant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ValaisEat/DAL/ICustomerDB.cs
@@ -0,0 +1,20 @@
+﻿using DTO;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public interface ICustomerDB
+    {
+        IConfiguration Configuration { get; }
+
+        List<Customer> GetCustomer();
+        Customer GetCustomer(int id);
+        Customer AddCustomer(Customer customer);
+        int UpdateCustomer(Customer customer);
+        int DeleteCustomer(int idCustomer);
+
+        
+    }
+}
